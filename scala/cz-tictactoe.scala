80fbe1b612251701314996938f60faa090c260de
Carsten Zimmermann <cz@aegisnet.de>
2019-01-04 00:41:56 +0100

    Play with constructor overloading

--- scala/cz-tictactoe.scala
+++ scala/cz-tictactoe.scala
@@ -3,6 +3,16 @@ class Player(val name: String, val marker: Char)
 class Board(val player1: Player, val player2: Player) {
   private val N = "·" // Blank value to initialize board with
 
+  // TODO: make sure both players have different `marker` values
+
+  /**
+   * Allows construction like so:
+   *    new Board(("John", 'X'), ("Jane", 'O'))
+   */
+  def this(player1: (String, Char), player2: (String, Char)) {
+    this(new Player(player1._1, player1._2), new Player(player2._1, player2._2))
+  }
+
   var fields = Array.tabulate(3,3)( (x,y) => N )
 
   // Somewhat clunky way to flip the coin on who's gonna start the game?
@@ -22,6 +32,7 @@ class Board(val player1: Player, val player2: Player) {
 }
 
 println
-var board = new Board(new Player("Batman", 'B'), new Player("Superman", 'S'))
+// var board = new Board(new Player("Batman", 'B'), new Player("Superman", 'S'))
+var board = new Board(("Batman", 'B'), ("Superman", 'S'))
 board.printField
 board.playerAtTurn
