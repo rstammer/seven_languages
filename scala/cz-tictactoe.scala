c5495d21237ec7d2f2a27e9941720b373517ffed
Carsten Zimmermann <cz@aegisnet.de>
2019-01-04 00:17:36 +0100

    Initialize TTT board w/ players

---
+++ scala/cz-tictactoe.scala
@@ -0,0 +1,27 @@
+class Player(val name: String, val marker: Char)
+
+class Board(val player1: Player, val player2: Player) {
+  private val N = "·" // Blank value to initialize board with
+
+  var fields = Array.tabulate(3,3)( (x,y) => N )
+
+  // Somewhat clunky way to flip the coin on who's gonna start the game?
+  private var _playerAtTurn = scala.util.Random.shuffle(List[Player](player1, player2)).take(1)(0)
+
+  def playerAtTurn = println("It's " + _playerAtTurn.name + "(" + _playerAtTurn.marker + ")'s turn.")
+
+  def printField(): Unit = {
+    for(i <- 0 until 3) {
+      for(j <- 0 until 3) {
+        print(" " + fields(i)(j) + " ")
+      }
+      println
+      println
+    }
+  }
+}
+
+println
+var board = new Board(new Player("Batman", 'B'), new Player("Superman", 'S'))
+board.printField
+board.playerAtTurn
