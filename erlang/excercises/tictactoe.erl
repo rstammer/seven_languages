9e37d98361f5571a5839d61d122b21c91d0e3ef6
Robin Neumann <robin.neumann@posteo.de>
2019-02-10 19:33:48 +0100

    Add comments

--- erlang/excercises/tictactoe.erl
+++ erlang/excercises/tictactoe.erl
@@ -1,5 +1,5 @@
 -module(tictactoe).
--export([result/1]).
+-export([winner/1]).
 
 transpose([
            [A, B, C],
@@ -16,18 +16,30 @@ diagonal_winner_row([
                      [P, _, _],
                      [_, P, _],
                      [_, _, P]
-                    ]) -> true;
+                    ]) -> P;
 
-diagonal_winner_row(_) -> false.
+diagonal_winner_row(_) -> no_winner.
 
 
-horizontal_winner([]) -> false;
-horizontal_winner([ [X, X, X] | _]) -> true;
+horizontal_winner([]) -> no_winner;
+horizontal_winner([ [X, X, X] | _]) -> X;
 horizontal_winner([ [_, _, _] | Tail]) -> horizontal_winner(Tail).
 
-result(Board) -> 
+%%% Accepts a list of 3 3-element-sized lists that
+%%% represent a classical tic-tac-toe board:
+%%%
+%%% X | X | O   is represented by [[x, x, o], [o, x, o], [o, o, x]]
+%%% O | X | O
+%%% O | O | X
+%%%
+%%% Returns winner player atom if game has a winner, no_winner if game has no winner
+%%% yet and returns cat in case of a remis.
+%%%
+%%%
+%%% TODO: Implement the cat case 🐱.
+winner(Board) ->
   HorizontalWinner    = horizontal_winner(Board),
   VerticalWinner      = horizontal_winner(transpose(Board)),
   LeftDiagonalWinner  = diagonal_winner_row(Board),
   RightDiagonalWinner = diagonal_winner_row(transpose(Board)),
-  HorizontalWinner or VerticalWinner or LeftDiagonalWinner or RightDiagonalWinner.
+  hd([HorizontalWinner, VerticalWinner, LeftDiagonalWinner, RightDiagonalWinner]).
