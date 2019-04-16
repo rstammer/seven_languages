497f0cf558c543547008e33046ac80b3939d06a3
Robin Neumann <robin.neumann@posteo.de>
2019-04-16 21:58:27 +0200

    Add some exemplaric solutions for Day 1 exercises for Haskell Chapter

---
+++ haskell/day1.hs
@@ -0,0 +1,23 @@
+﻿
+module Day1 where
+  import Data.List
+
+  -- Philosophical question:
+  -- Are all elements of an empty list even?
+  -- Are all elements of an empty list odd?
+  -- Following my naive sense of formal logic,
+  -- both has to be true.
+  allEven1 [] = True
+  allEven1 (x:xs) = (mod x 2) == 0 && (allEven1 xs)
+
+  allEven2 list = (length odds) == 0 where
+    odds = filter isOdd list
+    isOdd x = mod x 2 /= 0
+
+  reverseList [] = []
+  reverseList list = (reverse $ tail list) ++ [head list]
+
+  colorCombos colors = nubBy glue [(x,y) | x <- colors, y <- colors, x /= y] where
+    glue x y = fst(x) == snd(y) && fst(y) == snd(x)
+
+  multiplicationTable a b = [(x, y, x*y) | x <- [a..b], y <- [a..b]]
